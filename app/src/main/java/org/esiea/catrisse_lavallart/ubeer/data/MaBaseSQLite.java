de20912b2ca687a3bf42879ae4276b5c44c5f922
tinmar <martin.lavallart@gmail.com>
2018-12-13 18:30:48 +0100

    mise en place de la BDD

---
+++ app/src/main/java/org/esiea/catrisse_lavallart/ubeer/data/MaBaseSQLite.java
@@ -0,0 +1,41 @@
+package org.esiea.catrisse_lavallart.ubeer.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+
+public class MaBaseSQLite extends SQLiteOpenHelper {
+
+    private static final String TABLE_BAR = "table_bar";
+    private static final String COL_ID = "ID";
+    private static final String COL_NAME = "name";
+    private static final String COL_ADDRESS = "address";
+    private static final String COL_ISOPEN = "isopen";
+    private static final String COL_RANK = "rank";
+    private static final String COL_URL = "url";
+
+
+    private static final String CREATE_BDD = "CREATE TABLE " + TABLE_BAR + " ("
+            + COL_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + COL_NAME + " TEXT NOT NULL, "
+            + COL_ADDRESS + " TEXT," + COL_ISOPEN+ " TEXT ," + COL_RANK + " TEXT ," + COL_URL + " TEXT);";
+
+    public MaBaseSQLite(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //on crée la table à partir de la requête écrite dans la variable CREATE_BDD
+        db.execSQL(CREATE_BDD);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //On peut faire ce qu'on veut ici moi j'ai décidé de supprimer la table et de la recréer
+        //comme ça lorsque je change la version les id repartent de 0
+        db.execSQL("DROP TABLE " + TABLE_BAR + ";");
+        onCreate(db);
+    }
+
+}
--- app/src/main/java/org/esiea/catrisse_lavallart/ubeer/model/Bar.java
+++ app/src/main/java/org/esiea/catrisse_lavallart/ubeer/model/Bar.java
@@ -24,30 +24,45 @@ public class Bar implements Serializable {
         this.imageUrl=imageUrl;
 
     }
+    public Bar()
+    {
+    }
 
 
     public String getName()
     {
         return this.name;
     }
-
+    public void setName(String nname){
+        this.name = nname;
+    }
     public String getAddress()
     {
         return this.address;
     }
-
+    public void setAddress(String naddress){
+        this.address = naddress;
+    }
     public String getIsOpen()
     {
         return this.isOpen;
     }
-
+    public void setIsopen(String nIsopen){
+        this.isOpen = nIsopen;
+    }
     public String getRank()
     {
         return this.rank;
     }
+    public void setRank(String nrank){
+        this.rank = nrank;
+    }
     public String getUrl()
     {
         return this.imageUrl;
+    }
+    public void setUrl(String nurl){
+        this.imageUrl = nurl;
     }
 
     @Override
